dd49910f2267370aca6c2e49c1f9f971e484806a
tofudd <bartlomiej.tota@outlook.com>
2020-08-14 22:11:20 +0200

    Example projects

---
+++ RadioBrowser.Example/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using RadioBrowser.Models;
+
+namespace RadioBrowser.Example
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainAsync().Wait();
+        }
+
+        private static async Task MainAsync()
+        {
+            // Initialization
+            var radioBrowser = new RadioBrowser();
+
+            // Searching by name
+            var searchByName = await radioBrowser.Search.ByNameAsync("tofu radio");
+            Console.WriteLine(searchByName.First().Name); 
+            Console.WriteLine("");
+            
+            // Advanced searching
+            var advancedSearch = await radioBrowser.Search.AdvancedAsync(new AdvancedSearchOptions
+            {
+                Language = "english",
+                TagList = "news"
+            });
+            Console.WriteLine(advancedSearch.First().Name);
+            Console.WriteLine("");
+            
+            // Getting top stations
+            var topByVotes = await radioBrowser.Stations.GetByVotesAsync(10);
+
+            foreach (var station in topByVotes)
+            {
+                Console.WriteLine(station.Name);
+            }
+            Console.WriteLine("");
+
+            // Getting codecs list
+            var codecs = await radioBrowser.Lists.GetCodecsAsync();
+            foreach (var codec in codecs)
+            {
+                Console.WriteLine($"{codec.Name} - {codec.Stationcount}");
+            }
+        }
+    }
+}
